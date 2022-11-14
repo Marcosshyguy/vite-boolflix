c90ad36944c567c87885dd6183b283e35e514bb2
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2022-11-14 15:21:25 +0100

    create AppMovieGrid

---
+++ src/components/AppMovieGrid.vue
@@ -0,0 +1,35 @@
+<script>
+import {store} from "../store"
+
+export default{
+  data(){
+    return{
+      store
+    }
+  },
+  methods:{
+
+  },
+}
+</script>
+
+<template>
+    <div class="main-container">
+        <div class="row">
+            <div class="col">fgf</div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+ 
+.row{
+    display: flex;
+    justify-content: start;
+
+        .col{
+            width: calc(100% / 5);
+        }
+}
+
+</style>

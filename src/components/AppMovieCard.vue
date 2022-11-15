29556bcd890efa7d45466bbf8e748bfa1298e38c
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2022-11-15 11:44:22 +0100

    fixed props with a single object

--- src/components/AppMovieCard.vue
+++ src/components/AppMovieCard.vue
@@ -11,14 +11,15 @@ export default{
 
   },
   props: {
-    title : String,
-    original_title : String,
-    overview :String,
-    vote_average: Number,
-    original_language: String,
-    name: String,
-    original_name: String,
-    poster_path: String
+    // title : String,
+    // original_title : String,
+    // overview :String,
+    // vote_average: Number,
+    // original_language: String,
+    // name: String,
+    // original_name: String,
+    // poster_path: String
+    element : Object
   }
 }
 </script>
@@ -26,47 +27,47 @@ export default{
 <template>
     <div class="card">
         <div>
-            <img :src="`https://image.tmdb.org/t/p/w342${poster_path}`" :alt="original_title" v-if="poster_path">
+            <img :src="`https://image.tmdb.org/t/p/w342${element.poster_path}`" :alt="element.original_title" v-if="element.poster_path">
             <p v-else>Immagine non trovata</p>
         </div>
         <p><strong>Lingua originale: </strong>
-            <span v-if="original_language === 'en'">Bye Bye</span>
+            <span v-if="element.original_language === 'en'">Bye Bye</span>
             <span v-else>kawai</span>
         </p>
-        <p><strong>Titolo: </strong>{{title}}{{name}}</p>
-        <p><strong>Titolo originale: </strong>{{original_title}}{{original_name}}</p>
-        <p><strong>Trama: </strong>{{overview}}</p>
-        <p><strong>Voto: </strong>{{vote_average}}</p>
+        <p><strong>Titolo: </strong>{{element.title}}{{element.name}}</p>
+        <p><strong>Titolo originale: </strong>{{element.original_title}}{{element.original_name}}</p>
+        <p><strong>Trama: </strong>{{element.overview}}</p>
+        <p><strong>Voto: </strong>{{element.vote_average}}</p>
         <span>Rating 
-            <span v-if="vote_average > 0 && vote_average <= 2" >
+            <span v-if="element.vote_average > 0 && element.vote_average <= 2" >
                 <i class="fa-solid fa-star"></i>
                 <i class="fa-regular fa-star"></i>
                 <i class="fa-regular fa-star"></i>
                 <i class="fa-regular fa-star"></i>
                 <i class="fa-regular fa-star"></i>
             </span>
-            <span v-else-if="vote_average > 2 && vote_average <= 4">
+            <span v-else-if="element.vote_average > 2 && element.vote_average <= 4">
                 <i class="fa-solid fa-star"></i>
                 <i class="fa-solid fa-star"></i>
                 <i class="fa-regular fa-star"></i>
                 <i class="fa-regular fa-star"></i>
                 <i class="fa-regular fa-star"></i>
             </span>
-            <span v-else-if="vote_average > 4 && vote_average <= 6">
+            <span v-else-if="element.vote_average > 4 && element.vote_average <= 6">
                 <i class="fa-solid fa-star"></i>
                 <i class="fa-solid fa-star"></i>
                 <i class="fa-solid fa-star"></i>
                 <i class="fa-regular fa-star"></i>
                 <i class="fa-regular fa-star"></i>
             </span>
-            <span v-else-if="vote_average > 6 && vote_average <= 8">
+            <span v-else-if="element.vote_average > 6 && element.vote_average <= 8">
                 <i class="fa-solid fa-star"></i>
                 <i class="fa-solid fa-star"></i>
                 <i class="fa-solid fa-star"></i>
                 <i class="fa-solid fa-star"></i>
                 <i class="fa-regular fa-star"></i>
             </span>
-            <span v-else-if="vote_average > 8 && vote_average <= 10">
+            <span v-else-if="element.vote_average > 8 && element.vote_average <= 10">
                 <i class="fa-solid fa-star"></i>
                 <i class="fa-solid fa-star"></i>
                 <i class="fa-solid fa-star"></i>

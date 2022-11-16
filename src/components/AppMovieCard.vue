81dcb7c67935bcc84ff21101f8dc0c6ff9059fda
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2022-11-16 15:55:12 +0100

    put flag on original leanguage section and correct the voteavarage

--- src/components/AppMovieCard.vue
+++ src/components/AppMovieCard.vue
@@ -5,6 +5,8 @@ export default{
   data(){
     return{
       store
+    //   this array collect all the lenguages to check when we put flags
+    //   [it,en]
     }
   },
   methods:{
@@ -37,21 +39,28 @@ export default{
                     <img src="../assets/images/image-not-found.jpeg" alt="Image Not Found" v-else>
             </div>
             <div class="card-infos">
-                <p><strong>Lingua originale: {{element.original_language}}</strong>
-                    <!-- <span v-if="element.original_language === 'en'">Bye Bye</span>
-                    <span v-else>kawai</span> -->
-                </p>
+                <div>
+                    <!-- one way to add images the other one is to collect all the lenguages inside an array and chec if they are insede of it but the issue is to check the file type -->
+                    <!-- <img v-if="availableFlags.includes(item.original_language)"
+                        :src="getImgUrl(`${item.original_language}.png`)"> -->
+                    <span><strong>Lingua originale: </strong>
+                        <span v-if="element.original_language === 'en'"><img :src="getImgPathJpg(element.original_language)" alt=""></span>
+                        <span v-else-if="element.original_language === 'it'"><img :src="getImgPathJpg(element.original_language)" alt=""></span>
+                        <span v-else> {{element.original_language}}</span> 
+                    </span>
+
+                </div>
                 <p><strong>Titolo: </strong>{{element.title}}{{element.name}}</p>
                 <p><strong>Titolo originale: </strong>{{element.original_title}}{{element.original_name}}</p>
                 <p><strong>Trama: </strong>{{element.overview}}</p>
-                <p><strong>Voto: </strong>{{element.vote_average}}</p>
+                <p><strong>Voto: </strong>{{Math.ceil(element.vote_average / 2)}}</p>
                 <span>Rating 
                     <!-- cicle the star rating according to the vote_avarage -->
                     <!-- fast way -->
-                    <span v-for="fullStar in (Math.floor(element.vote_average / 2))">
+                    <span v-for="fullStar in (Math.ceil(element.vote_average / 2))">
                         <i class="fa-solid fa-star"></i>
                     </span>
-                    <span v-for="emptyStar in (5 - (Math.floor(element.vote_average / 2)))">
+                    <span v-for="emptyStar in (5 - (Math.ceil(element.vote_average / 2)))">
                         <i class="fa-regular fa-star"></i>
                     </span>
                     <!-- slow way -->
@@ -102,6 +111,7 @@ export default{
 .card-container{
     height: 100%;
     width: 100%;
+    perspective: 1000px;
     .card{
         height: 100%;
         width:100%;
@@ -109,6 +119,7 @@ export default{
         position: relative;
         transition: all 1s;
         transform-style: preserve-3d;
+        
         
             .card-image{
                 height: 100%;
@@ -137,14 +148,26 @@ export default{
                 z-index: 1;
                 color: white;
                 text-align: center;
-                transform: rotateY(180deg);
+                transform: rotateX(180deg);
                 overflow-x: auto;
+
+                & > *{
+                    margin-bottom: .5rem;
+                }
+
+                img{
+                    width: 5%;
+                }
+
+                .fa-solid{
+                    color: yellow;
+                }
             }
 
             
     }
     &:hover .card{
-            transform: rotateY(180deg);
+            transform: rotateX(180deg);
         }
 }
 

f21cd1a7b1cbd081c5aa7b5b91b67ebde81eb71e
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2022-11-16 17:40:09 +0100

    add genreList

--- src/store.js
+++ src/store.js
@@ -3,9 +3,11 @@ import {reactive} from 'vue';
 export const store = reactive({
     movieApi : "https://api.themoviedb.org/3/search/movie",
     tvShowsApi: "https://api.themoviedb.org/3/search/tv",
+    genreApi:"https://api.themoviedb.org/3/genre/tv/list",
     apiKey : "7e1f56d2ef367d1a473c28d6c3add2f9",
     moviesArray : [],
     tvShowsArray :[],
+    genreList:"",
     searchResult : "",
     hideSearchBar:false
 })
